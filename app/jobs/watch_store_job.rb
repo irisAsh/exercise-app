4ca906f7eb8b1ca0fa06a7762248dd20e4d9a5d7
irisAsh <043takotoka@gmail.com>
2018-04-19 15:49:08 +0900

    Fix schedule

--- app/jobs/watch_store_job.rb
+++ app/jobs/watch_store_job.rb
@@ -2,6 +2,8 @@ class WatchStoreJob < ApplicationJob
   queue_as :default
 
   def perform
+    p '========== Start WatchStoreJob =========='
+
     uri = URI.parse 'https://rss.itunes.apple.com/api/v1/jp/ios-apps/new-apps-we-love/all/10/explicit.json'
 
     http = Net::HTTP.new uri.host, uri.port
@@ -20,11 +22,12 @@ class WatchStoreJob < ApplicationJob
     notifier = Slack::Notifier.new('https://hooks.slack.com/services/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')
     notifier.ping <<-EOS.strip_heredoc
 
-      IOS #{feed["title"]} 10選
+      ~~~~~~~~~~~~~~~~~~~~ IOS #{feed["title"]} 10選 ~~~~~~~~~~~~~~~~~~~~
     EOS
 
     results.each { |result| showResult notifier, result }
 
+    p '========== End WatchStoreJob =========='
   end
 
   def showResult(notifier, result)
